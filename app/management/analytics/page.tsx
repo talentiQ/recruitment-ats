486a5875548af3e90b123f1064124280dc8fa870
Kunal Bhatia <kunal@talenti.biz>
2026-03-08 18:53:48 +0530

    all

--- app/management/analytics/page.tsx
+++ app/management/analytics/page.tsx
@@ -6,6 +6,8 @@ import { useRouter } from 'next/navigation'
 import { supabaseAdmin } from '@/lib/supabaseAdmin'
 import DashboardLayout from '@/components/DashboardLayout'
 import * as XLSX from 'xlsx'
+console.log('SERVICE KEY PRESENT:', !!process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY)
+console.log('SERVICE KEY VALUE:', process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY?.slice(0, 20))
 
 interface RecruiterProgress {
   recruiter_id: string
